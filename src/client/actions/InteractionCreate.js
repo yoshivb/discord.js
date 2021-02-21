d7fe86dfff458fa173fc0c8b2e7c3edb90eeb289
Yoshi van Belkom <yoshivb@users.noreply.github.com>
2021-02-21 19:33:47 +0100

    Fix for dms

--- src/client/actions/InteractionCreate.js
+++ src/client/actions/InteractionCreate.js
@@ -14,13 +14,14 @@ class InteractionCreateAction extends Action {
   async handle(data) {
     const client = this.client;
     const guild = client.guilds.cache.get(data.guild_id);
+    const authorid = guild ? data.member.user.id : data.user.id;
     const interaction = {
       id: data.id,
       channel: client.channels.cache.get(data.channel_id),
       guild: guild,
       token: data.token,
       member: guild ? guild.members.cache.get(data.member.user.id) || (await guild.members.fetch(data.member.user.id)) || null : null,
-      author: client.users.cache.get(data.member.user.id) || (await client.users.fetch(data.member.user.id)) || null,
+      author: client.users.cache.get(authorid) || (await client.users.fetch(authorid)) || null,
       name: data.data.name,
       content: data.data.options ? parseContent(data.data.options) : "",
       createdTimestamp: SnowflakeUtil.deconstruct(data.id).timestamp,
